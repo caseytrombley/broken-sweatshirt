e34930a291a6eb2f52b24aef631e31ccd1ef1f41
Casey Trombley <casey@algaecal.com>
2024-02-23 12:14:49 -0500

    add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1 class="logo">Broken Sweatshirt</h1>
+    <button @click="playAudio">Click Here!</button>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  methods: {
+    playAudio() {
+      const audio = new Audio(require('@/assets/yeah.mp3'));
+      audio.play();
+    }
+  }
+}
+</script>
