b8098f1550676e030ad657360a1e5110b351156a
Casey Trombley <casey@algaecal.com>
2024-02-23 11:34:01 -0500

    push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AudioButton from './components/AudioButton.vue'
+
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="logo">Broken Sweatshirt</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <audio-button />
 </template>
 
 <style scoped>
---
+++ src/components/AudioButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="play">Click Me!</button>
+</template>
+
+<script>
+import { useSound } from '@vueuse/sound'
+import buttonSfx from '../assets/yeah.mp3'
+
+export default {
+  setup() {
+    const { play } = useSound(buttonSfx)
+
+    return {
+      play,
+    }
+  },
+}
+</script>
